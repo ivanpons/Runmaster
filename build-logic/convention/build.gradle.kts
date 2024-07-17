f0bd6448da10f45d226a23b72199079d705189e4
ivanpons <llimapons@gmail.com>
2024-07-17 17:42:29 +0200

    feat(app): add first convention plugin

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,24 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.llimapons.runmaster.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
+
+
+gradlePlugin{
+    plugins{
+        register("androidApplication") {
+            id = "runmaster.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+    }
+}
