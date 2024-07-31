f097fbcd90526613a51952f3882f666405f0d886
ivanpons <llimapons@gmail.com>
2024-07-31 18:47:59 +0200

    feat(run): fix inyection

--- run/presentation/src/main/java/com/llimapons/presentation/di/RunPresentationModule.kt
+++ run/presentation/src/main/java/com/llimapons/presentation/di/RunPresentationModule.kt
@@ -3,12 +3,13 @@ package com.llimapons.presentation.di
 import com.llimapons.presentation.run_active.ActiveRunViewModel
 import com.llimapons.presentation.run_overview.RunOverviewViewModel
 import com.llimapons.run.domain.RunningTracker
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val runPresentationModule = module {
     singleOf(::RunningTracker)
 
-    singleOf(::RunOverviewViewModel)
-    singleOf(::ActiveRunViewModel)
+    viewModelOf(::RunOverviewViewModel)
+    viewModelOf(::ActiveRunViewModel)
 }
